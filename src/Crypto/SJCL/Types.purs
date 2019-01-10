fd0acc9a23a10696b8c5b6f39a4d677401185d9a
Athan Clark <athan.clark@localcooking.com>
2019-01-09 23:29:57 -0700

    first codec, untested

--- src/Crypto/SJCL/Types.purs
+++ src/Crypto/SJCL/Types.purs
@@ -1,22 +1,50 @@
 module Crypto.SJCL.Types
   ( BitArray
   , bitLength
+  , bitSlice
+  , byteswapM
+  , clamp
+  , extract
+  , getPartial
+  , partial
+  , partial'
+  , Word
+  , BitLength
   ) where
 
+
+import Prelude (class Eq, class Semigroup)
+import Data.Function.Uncurried (Fn2, Fn3, runFn3, runFn2)
+
+
+type BitLength = Int
 
 
 newtype BitArray = BitArray (Array Word)
 
-newtype Word = Word Number
+instance eqBitArray :: Eq BitArray where
+  eq = runFn2 equalImpl
+
+instance semigroupBitArray :: Semigroup BitArray where
+  append = runFn2 concatImpl
+
+
+type Word = Number
 
 
 foreign import bitLength :: BitArray -> Int
 
 foreign import bitSliceImpl :: Fn3 BitArray Int Int BitArray
 
+bitSlice :: BitArray -> Int -> Int -> BitArray
+bitSlice = runFn3 bitSliceImpl
+
 foreign import byteswapM :: BitArray -> BitArray
 
 foreign import clampImpl :: Fn2 BitArray Int BitArray
+
+clamp :: BitArray -> Int -> BitArray
+clamp = runFn2 clampImpl
 
 foreign import concatImpl :: Fn2 BitArray BitArray BitArray
 
@@ -24,8 +52,17 @@ foreign import equalImpl :: Fn2 BitArray BitArray Boolean
 
 foreign import extractImpl :: Fn3 BitArray Int Int Word
 
+extract :: BitArray -> Int -> Int -> Word
+extract = runFn3 extractImpl
+
 foreign import getPartial :: Word -> Int
 
 foreign import partialImpl :: Fn2 Int Word Word
 
+partial :: Int -> Word -> Word
+partial = runFn2 partialImpl
+
 foreign import partial_Impl :: Fn2 Int Word Word
+
+partial' :: Int -> Word -> Word
+partial' = runFn2 partial_Impl
